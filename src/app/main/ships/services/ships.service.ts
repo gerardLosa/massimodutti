2c4a5fbbc530c41c6dec8b71721369ecbda727d0
gerardLosa <gerardlosa@gmail.com>
2020-02-28 22:12:00 +0100

    Get ships from API

--- src/app/main/ships/services/ships.service.ts
+++ src/app/main/ships/services/ships.service.ts
@@ -1,13 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class ShipsService {
-    getShips(): Observable<Array<any>> {
-        return of(null);
+    constructor(private httpClient: HttpClient) { }
+
+
+    public getShips(url?: string): Observable<any> {
+        if (!url) {
+            url = 'https://swapi.co/api/starships/';
+        }
+        return this.httpClient.get<any>(url)
+            .pipe(
+                map(
+                    res => res
+                ),
+                catchError(
+                    error => {
+                        console.log('Http Error: ', error);
+                        return of(null);
+                    }
+                )
+            );
     }
 
 }
